5595aae2258722fa1896b9107da0f1b6a53fcc17
marcelobarbieri <marcelodpbarbieri@gmail.com>
2023-02-10 08:24:29 -0300

    Decision and Iteration - Demo switch Statement

--- README.md
+++ README.md
@@ -1466,6 +1466,92 @@ swith (age)
 - First "true" will get executed (top to bottom)
 - Default can be placed wherever we want, always evaluated last
 
+</details>
+
+<!-- #endregion -->
+
+<!-- #region Using switch Statement -->
+
+<details>
+<summary>Using switch Statement</summary>
+
+Program.cs
+
+```c#
+using System.Diagnostics.Metrics;
+using System.Net.Http.Headers;
+
+Console.WriteLine("Enter the age of the new candidate: ");
+int age = int.Parse(Console.ReadLine());
+
+switch (age)
+{
+    case < 18:
+        Console.WriteLine("Too young to apply");
+        Console.WriteLine("Send email to candidate.");
+        break;
+    case > 65:
+        Console.WriteLine("Sorry, the selected age is too old");
+        Console.WriteLine("Send email to candidate.");
+        break;
+    default:
+        Console.WriteLine("Great, you can now start with the application!");
+        break;
+}
+
+//Enter the age of the new candidate:
+//25
+//Great, you can now start with the application!
+```
+
+Program.cs
+
+```c#
+switch (age)
+{
+    case < 18:
+    case > 65:
+        Console.WriteLine("Sorry, your age is not within the range we are looking for");
+        break;
+    case 23:
+        Console.WriteLine("Wow, exactly what we are looking for");
+        break;
+    default:
+        Console.WriteLine("Great, you can now start with the application!");
+        break;
+}
+```
+
+Program.cs
+
+```c#
+Console.WriteLine("Choose the action you want to do:");
+Console.WriteLine("1. Add employee");
+Console.WriteLine("2. Update employee");
+Console.WriteLine("3. Delete employee");
+string selectedAction = Console.ReadLine();
+
+switch(selectedAction)
+{
+    case "1":
+        Console.WriteLine("Adding new employee...");
+        break;
+    case "2":
+        Console.WriteLine("Updating employee...");
+        break;
+    case "3":
+        Console.WriteLine("Deleting employee...");
+        break;
+    default:
+        Console.WriteLine("Invalid input");
+        break;
+}
+```
+
+<br/>
+
+</details>
+
 <!-- #endregion -->
 
 <!-- #endregion -->
--- projects/BethanyPieShopHRM/BethanyPieShopHRM/Program.cs
+++ projects/BethanyPieShopHRM/BethanyPieShopHRM/Program.cs
@@ -1,46 +1,61 @@
 ﻿using System.Diagnostics.Metrics;
+using System.Net.Http.Headers;
 
 Console.WriteLine("Enter the age of the new candidate: ");
 int age = int.Parse(Console.ReadLine());
 
-if (age < 18)
+switch (age)
 {
-    Console.WriteLine("Too young to apply");
-    Console.WriteLine("Send email to candidate.");
+    case < 18:
+        Console.WriteLine("Too young to apply");
+        Console.WriteLine("Send email to candidate.");
+        break;
+    case > 65:
+        Console.WriteLine("Sorry, the selected age is too old");
+        Console.WriteLine("Send email to candidate.");
+        break;
+    default:
+        Console.WriteLine("Great, you can now start with the application!");
+        break;
 }
-else if (age > 65)
-{
-    Console.WriteLine("Sorry, the selected age is too old");
-    Console.WriteLine("Send email to candidate.");
-}
-else
-{
-    Console.WriteLine("Great, you can now start with the application!");
-}
-
-//Enter the age of the new candidate:
-//15
-//Too young to apply
-//Send email to candidate.
-
-//Enter the age of the new candidate:
-//75
-//Sorry, the selected age is too old
-//Send email to candidate.
 
 //Enter the age of the new candidate:
 //25
 //Great, you can now start with the application!
 
-DateTime today = DateTime.Now;
-bool endOfMonthPaymentStarted = false;
+switch (age)
+{
+    case < 18:
+    case > 65:
+        Console.WriteLine("Sorry, your age is not within the range we are looking for");
+        break;
+    case 23:
+        Console.WriteLine("Wow, exactly what we are looking for");
+        break;
+    default:
+        Console.WriteLine("Great, you can now start with the application!");
+        break;
+}
 
-if (today.Date.Day == 20)
+
+Console.WriteLine("Choose the action you want to do:");
+Console.WriteLine("1. Add employee");
+Console.WriteLine("2. Update employee");
+Console.WriteLine("3. Delete employee");
+string selectedAction = Console.ReadLine();
+
+switch(selectedAction)
 {
-    Console.WriteLine("Please start end-of-month employee payments");
+    case "1":
+        Console.WriteLine("Adding new employee...");
+        break;
+    case "2":
+        Console.WriteLine("Updating employee...");
+        break;
+    case "3":
+        Console.WriteLine("Deleting employee...");
+        break;
+    default:
+        Console.WriteLine("Invalid input");
+        break;
 }
-else if (today.Date.Day >= 25 && ! endOfMonthPaymentStarted)
-{
-    Console.WriteLine("Payments will be late!");
-}
-// else isn´t required!
